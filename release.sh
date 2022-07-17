b4a024350c771c58ddc41327cb4d0d1753ec439c
Joshua Paul Chan <joshpaulchan@gmail.com>
2022-07-17 16:49:31 -0400

    move cleanup to beginning of run

--- release.sh
+++ release.sh
@@ -1,3 +1,6 @@
+echo "cleaning up dir"
+rm app-infra-starter-*.tgz
+
 echo "building chart"
 gcloud auth print-access-token | helm registry login -u oauth2accesstoken --password-stdin https://us-central1-docker.pkg.dev
 helm package helm/
@@ -5,6 +8,3 @@ helm package helm/
 echo "pushing"
 helm push app-infra-starter-*.tgz oci://us-central1-docker.pkg.dev/joshpaulchan/helm-repo
 helm registry logout https://us-central1-docker.pkg.dev
-
-echo "cleaning up dir"
-rm app-infra-starter-*.tgz
